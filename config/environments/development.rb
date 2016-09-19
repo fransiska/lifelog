0533a09d2dc6f851d96286525b6fe53e07183b95
fransiska <>
2016-09-19 12:35:37 +0700

    add precompiled css

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,10 +1,16 @@
 Lifelog::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.cache_classes = true
+  config.assets.compile = true
+  config.assets.precompile =  ['*.js', '*.css', '*.css.erb'] 
+  config.serve_static_files = true
+  config.assets.digest = false
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+  # config.cache_classes = false
 
   # Do not eager load code on boot.
   config.eager_load = false
